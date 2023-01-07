d20c02d3fe9110324a7651a7a3e727dee1708994
Meghana-Parelli <113854127+Meghana-Parelli@users.noreply.github.com>
2023-01-07 19:57:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 model_nb = joblib.load('Ecom_fraud_det')
 st.title('Ecom_fraud_det') #creates a title in web app
-ip = st.number_input('Enter input:') #creates a text box in web app
+ip = st.text_input('Enter input:') #creates a text box in web app
 op = model_nb.predict([[ip]])
 if st.button('Predict'):
   st.title(op[0])
