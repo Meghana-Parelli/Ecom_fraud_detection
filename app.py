f5eb746e3686a345994a675fb320f5b9f8fb73c8
Meghana-Parelli <113854127+Meghana-Parelli@users.noreply.github.com>
2023-01-07 19:27:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import joblib
 model_nb = joblib.load('Ecom_fraud_det')
 st.title('Ecom_fraud_det') #creates a title in web app
 ip = st.text_input('Enter input:') #creates a text box in web app
-op = model_nb.predict([ip])
+op = model_nb.predict([input])
 if st.button('Predict'):
   st.title(op[0])
-  
+               
+             
